a785ed9b923d46f57d2635e2376c9c425ec87065
Anisija21 <77109056+Anisija21@users.noreply.github.com>
2022-05-19 12:10:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("Cik gadu izglītība jums ir? "))
+
+if a == 12:
+    print("Jums ir vidējā izglītība un tas nozīmē, ka jūs varat iestāties universitātēs, piemēram LU, RTU, RSU un RISEBA")
+
+if a < 12:
+    print("Jums nav vidējās izglītības(nav pabeigta vidusskola - 12.klases) ")
+    print("Mēs piedāvājam pabeigt mācīšanās tehnikumos, piemēram RTK un pēc tam iestāties universitātē - LU, RTU, RSU, RISEBA. Vairāk informācijas mūsu mājaslapā https://ConstantSquigglyAutoexec.anisija.repl.co")
+    
